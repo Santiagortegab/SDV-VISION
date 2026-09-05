3892a549f4eb4fed8586a4e0850fac9d8334841e
Santiagortegab <santiago.2005bur@gmail.com>
2026-09-05 14:01:50 -0600

    Nodos ROS2

---
+++ src/sdv_pkg/launch/sdv.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='sdv_pkg',
+            executable='video_pub',
+            name='publicador_video',
+            output='screen'
+        ),
+        Node(
+            package='sdv_pkg',
+            executable='video_sub',
+            name='suscriptor_video',
+            output='screen'
+        )
+    ])
